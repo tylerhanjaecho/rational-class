3a26f58209ae11f741c6bfae7b77b3996888c1ea
Tyler Cho <99361716+tylerhanjaecho@users.noreply.github.com>
2022-02-11 20:41:15 +0000

    100 point version

--- Main.java
+++ Main.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the numerator ---->");
-		int numerator = scanner.nextInt();
-		System.out.println("Enter the denominator ---->");
-		int denominator =  scanner.nextInt();
-		Rational rational = new Rational(numerator, denominator);
-		System.out.println(rational.getRational() + " equals " + rational.getDecimal());
-		System.out.println("and reduces to " + rational.getSimplified());
+		System.out.println("Enter the first numerator ---->");
+		int numerator1 = scanner.nextInt();
+		System.out.println("Enter the first denominator ---->");
+		int denominator1 =  scanner.nextInt();
+		Rational rational1 = new Rational(numerator1, denominator1);
+
+		System.out.println("Enter the second numerator ---->");
+		int numerator2 = scanner.nextInt();
+		System.out.println("Enter the second denominator ---->");
+		int denominator2 =  scanner.nextInt();
+		Rational rational2 = new Rational(numerator2, denominator2);
+
+		System.out.println(rational1.getRational() + " + " + rational2.getRational() + " = " + rational1.getSum(rational1, rational2));
+		System.out.println(rational1.getRational() + " - " + rational2.getRational() + " = " + rational1.getDiff(rational1, rational2));
+		System.out.println(rational1.getRational() + " * " + rational2.getRational() + " = " + rational1.getProduct(rational1, rational2));
+		System.out.println(rational1.getRational() + " / " + rational2.getRational() + " = " + rational1.getQuotient(rational1, rational2));
 	}
 }		
--- Rational.java
+++ Rational.java
@@ -9,12 +9,22 @@ class Rational {
 		this.denominator = denominator;
 	}
 
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public int getDenominator() {
+		return denominator;
+	}
+
 	public String getRational() {
 		return numerator + "/" + denominator;
 	}
+
 	public double getDecimal() {
 		return (double) numerator / (double) denominator;
 	}
+
 	public int getGCF() {
 		for (int i = 1; i <= Math.min(numerator, denominator); i++) {
 			if (numerator % i == 0 && denominator % i == 0) {
@@ -23,7 +33,40 @@ class Rational {
 		}
 		return gcf;
 	}
+
 	public String getSimplified() {
 		return numerator / getGCF() + "/" + denominator / getGCF();
+	}
+
+	public String getSum(Rational rational1, Rational rational2) {
+		int tempNumerator = rational1.getNumerator() * rational2.getDenominator() + rational2.getNumerator() * rational1.getDenominator();
+		int tempDenominator = rational1.getDenominator() * rational2.getDenominator();
+		Rational tempRational = new Rational(tempNumerator, tempDenominator);
+
+		return tempRational.getSimplified(); 
+	}
+
+	public String getDiff(Rational rational1, Rational rational2) {
+		int tempNumerator = rational1.getNumerator() * rational2.getDenominator() - rational2.getNumerator() * rational1.getDenominator();
+		int tempDenominator = rational1.getDenominator() * rational2.getDenominator();
+		Rational tempRational = new Rational(tempNumerator, tempDenominator);
+		
+		return tempRational.getSimplified(); 
+	}
+
+	public String getProduct(Rational rational1, Rational rational2) {
+		int tempNumerator = rational1.getNumerator() * rational2.getNumerator();
+		int tempDenominator = rational1.getDenominator() * rational2.getDenominator();
+		Rational tempRational = new Rational(tempNumerator, tempDenominator);
+		
+		return tempRational.getSimplified(); 
+	}
+
+	public String getQuotient(Rational rational1, Rational rational2) {
+		int tempNumerator = rational1.getNumerator() * rational2.getDenominator();
+		int tempDenominator = rational1.getDenominator() * rational2.getNumerator();
+		Rational tempRational = new Rational(tempNumerator, tempDenominator);
+		
+		return tempRational.getSimplified(); 
 	}
 }		
